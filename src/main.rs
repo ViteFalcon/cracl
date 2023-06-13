2963c37e4c86a522c3ed6e286d718a9ee31de9c5
Vite Falcon <vitefalcon@gmail.com>
2023-06-12 20:28:53 -0700

    Added code to parse login server endpoints.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::fs;
+use std::net::SocketAddr;
 
 use bevy::prelude::*;
 
-#[derive(Deserialize, PartialEq, Debug)]
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
+struct LoginServer {
+    name: String,
+    endpoint: SocketAddr,
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Debug)]
 struct Config {
     version: String,
+    login_servers: Vec<LoginServer>,
 }
 
 fn main() {
     let config_yaml: String = fs::read_to_string("data/config.yaml").unwrap();
     let config: Config = serde_yaml::from_str(&config_yaml).unwrap();
     println!("Version: {}", config.version);
+    println!("Number of login servers: {}", config.login_servers.len());
     App::new().add_plugins(DefaultPlugins).run()
 }
