5f3f88a6ade02e27de83ed0f5425652a08896ac5
Vite Falcon <vitefalcon@gmail.com>
2023-06-13 00:17:14 -0700

    Updated config to store the default BGM music and volume

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::net::SocketAddr;
+use std::str;
 
 use bevy::prelude::*;
+
+#[derive(Deserialize, Default, Serialize, PartialEq, Debug)]
+struct AudioSettings {
+    default_bgm: String,
+    bgm_volume: f32,
+    fx_volume: f32,
+}
 
 #[derive(Deserialize, Serialize, PartialEq, Debug)]
 struct LoginServer {
@@ -10,16 +19,48 @@ struct LoginServer {
     endpoint: SocketAddr,
 }
 
-#[derive(Deserialize, Serialize, PartialEq, Debug)]
+impl Default for LoginServer {
+    fn default() -> Self {
+        LoginServer {
+            name: "localhost".parse().expect("Failed to set default hostname"),
+            endpoint: "127.0.0.1:6900"
+                .parse::<SocketAddr>()
+                .expect("Failed to parse default endpoint"),
+        }
+    }
+}
+
+#[derive(Deserialize, Default, Serialize, PartialEq, Debug)]
 struct Config {
     version: String,
+    audio: AudioSettings,
     login_servers: Vec<LoginServer>,
 }
 
+#[derive(Resource, Default)]
+struct State {
+    config: Config,
+    bgm: Handle<AudioSink>,
+}
+
 fn main() {
-    let config_yaml: String = fs::read_to_string("data/config.yaml").unwrap();
-    let config: Config = serde_yaml::from_str(&config_yaml).unwrap();
-    println!("Version: {}", config.version);
-    println!("Number of login servers: {}", config.login_servers.len());
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_resource::<State>()
+        .add_startup_system(load_config)
+        .add_startup_system(load_bgm)
+        .run()
+}
+
+fn load_config(mut state: ResMut<State>) {
+    let config_yaml = fs::read_to_string("assets/cracl.cfg").expect("Failed to load config file");
+    state.config = serde_yaml::from_str(&config_yaml).unwrap();
+}
+
+fn load_bgm(mut state: ResMut<State>, asset_server: Res<AssetServer>, audio: Res<Audio>) {
+    let music = asset_server.load(&state.config.audio.default_bgm);
+    state.bgm = audio.play_with_settings(
+        music,
+        PlaybackSettings::LOOP.with_volume(state.config.audio.bgm_volume),
+    );
 }
